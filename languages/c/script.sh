b460aa2e9b2fc06009b9693b8e0ae655653f3b7a
LEGENDRE <legendre.gui@gmail.com>
2014-02-20 17:57:24 +0100

    fix timeout

--- languages/c/script.sh
+++ languages/c/script.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-timeout -k 5 -s 9 5 (gcc ./compil/code/code.c -o ./compil/code/code && /compil/code/code )
+timeout -k 5 -s 9 5 gcc ./compil/code/code.c -o ./compil/code/code && /compil/code/code
